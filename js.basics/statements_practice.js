89ba0cdae8095b83c0bb7256b26809a0e2925471
adisacodes <marian.adisa@student.moringaschool.com>
2026-03-25 23:09:01 +0300

    Remove unwanted folders and files

--- js.basics/statements_practice.js
+++
@@ -1,6 +0,0 @@
-function calculateIntrest(principal, rate, time) {
-    let intrest = principal * rate * time;
-    return intrest;
-}
-let totalIntrest = calculateIntrest(1000, 0.05, 3);
-console.log("Total intrest earned:", totalIntrest);
